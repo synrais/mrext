ab4027defc67ab5b88ad581ec3a139766013b047
synrais <78070154+synrais@users.noreply.github.com>
2025-09-06 09:46:13 +1000

    Update main.go

--- cmd/sam/main.go
+++ cmd/sam/main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/wizzomafizzo/mrext/pkg/gameindex"
+	"github.com/wizzomafizzo/mrext/pkg/gameindex"
+	"github.com/wizzomafizzo/mrext/pkg/run"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: SAM -gameindex [flags]")
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: SAM -gameindex [flags] | -run [flags]")
+		os.Exit(1)
+	}
 
-    cmd := os.Args[1]
-    args := os.Args[2:]
+	cmd := os.Args[1]
+	args := os.Args[2:]
 
-    switch cmd {
-    case "-gameindex":
-        gameindex.Run(args)
-    default:
-        fmt.Printf("Unknown tool: %s\n", cmd)
-        os.Exit(1)
-    }
+	switch cmd {
+	case "-gameindex":
+		gameindex.Run(args)
+	case "-run":
+		run.Run(args)
+	default:
+		fmt.Printf("Unknown tool: %s\n", cmd)
+		os.Exit(1)
+	}
 }
